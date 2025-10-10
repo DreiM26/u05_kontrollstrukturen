ed816bd33ed58f422088d0e573783a904895851c
Maximiliano Arenas <max.arenas@gmail.com>
2025-10-10 10:05:40 +0200

    Create ABCAnalyse as Excercise

---
+++ auf5.14/ABCAnalyse.java
@@ -0,0 +1,33 @@
+// u05_kontrollstruktur
+// auf5.14
+
+public class ABCAnalyse
+{
+   public static void main(String [] args)
+   {
+      char prioritaet = 'A';
+      
+      if(prioritaet == 'A')
+      {
+         System.out.println("hoch");
+      }
+      else
+      {
+         if(prioritaet == 'B')
+         {
+            System.out.println("mittel");
+         }
+         else
+         {
+            if(prioritaet == 'C')
+            {
+               System.out.println("niedrig");
+            }
+            else
+            {
+               System.out.println("unzulaessig Wert"); 
+            }
+         }
+      }
+   }
+}
