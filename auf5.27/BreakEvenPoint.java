a61ddcbe82b8bf768ed15761d9a16daf737e84df
Maximiliano Arenas <max.arenas@gmail.com>
2025-10-13 09:37:23 +0200

    Create Break-Even-Point with FOR Command

---
+++ auf5.27/BreakEvenPoint.java
@@ -0,0 +1,34 @@
+// u05_kontrollstruktur
+// auf5.27
+
+public class BreakEvenPoint
+{
+   public static void main(String [] args)
+   {
+      int verkaufspreis  = 1500;
+      int rohstoffkosten = 600;
+      int arbeitskosten  = 400;
+      int fixkosten      = 5000;
+      int maxstueckzahl  = 15;
+
+      int variableKosten = (rohstoffkosten + arbeitskosten); //*i=Stueckzahl
+      int gesamtkosten   = fixkosten + variableKosten;       //
+      int erloes         = verkaufspreis;                    //*i=Stueckzahl
+      int ueberschuss    = erloes - gesamtkosten;            //
+
+      System.out.println("Break-Even-Point");
+      System.out.println("================");
+
+      System.out.println("Verkaufspreis pro Stueck:      " + verkaufspreis);
+      System.out.println("Rohstoffkosten pro Stueck:     " + rohstoffkosten);
+      System.out.println("Arbeitskosten pro Stueck:      " + arbeitskosten);
+      System.out.println("Fixkosten pro Monat:           " + fixkosten);
+      System.out.println("Maximlae Stueckzahl pro Monat: " + maxstueckzahl);
+      System.out.println("");
+
+      for(int i = 1 ; i <= maxstueckzahl ; i++)
+      {
+         System.out.printf("Stueckzahl: %d  Gesamtkosten: %d   Erloes: %d   Ueberschuss: %d\n ", i, fixkosten + ((rohstoffkosten + arbeitskosten)*i) , verkaufspreis*i, ueberschuss);
+      }
+   }
+}
